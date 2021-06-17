71aa297d8cfa30507710e000d07bccd109c9a3d7
Goldcoders <goldcoders@protonmail.com>
2021-06-17 18:21:13 +0800

    update buxdotph.dart

--- lib/buxdotph.dart
+++ lib/buxdotph.dart
@@ -1,11 +1,9 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 class Buxdotph {
-  static const MethodChannel _channel =
-      const MethodChannel('buxdotph');
+  static const MethodChannel _channel = const MethodChannel('buxdotph');
 
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
