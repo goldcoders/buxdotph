d6d31b46e47c9bc3f89ac24d8f62723ae8db897e
Goldcoders <goldcoders@protonmail.com>
2021-06-17 18:31:38 +0800

    added get checkout

--- lib/buxdotph.dart
+++ lib/buxdotph.dart
@@ -1,12 +1,7 @@
 import 'dart:async';
 
-import 'package:flutter/services.dart';
-
-class Buxdotph {
-  static const MethodChannel _channel = const MethodChannel('buxdotph');
-
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+class Bux {
+  static Future<String?> get checkout async {
+    return 'OK';
   }
 }
