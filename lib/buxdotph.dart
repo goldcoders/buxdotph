494b5f987c30b512992619dbc526addbf9bc3a71
Goldcoders <goldcoders@protonmail.com>
2021-12-01 14:04:46 +0800

    show load and env , accessible in our package

--- lib/buxdotph.dart
+++ lib/buxdotph.dart
@@ -1,7 +1,7 @@
 /// A dart client for Bux API.
 library buxdotph;
 
-export 'package:dotenv/dotenv.dart';
+export 'package:dotenv/dotenv.dart' show load, env;
 
 export 'src/bux.dart';
 export 'src/headers.dart';
