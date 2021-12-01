272909e6efc0247ffdb24c73f51967b9b8c25666
Goldcoders <goldcoders@protonmail.com>
2021-12-02 05:51:33 +0800

    make class immutable

--- lib/src/bux.dart
+++ lib/src/bux.dart
@@ -1,52 +1,98 @@
 import 'dart:async';
 
-import 'package:buxdotph/src/headers.dart';
 import 'package:buxdotph/src/models/checkout_payload.dart';
 import 'package:buxdotph/src/services/bux_client.dart';
-import 'package:dotenv/dotenv.dart' show env;
+import 'package:equatable/equatable.dart';
 
-extension BoolParsing on String {
-  bool parseBool() {
-    if (toLowerCase() == 'true') {
-      return true;
-    } else if (toLowerCase() == 'false') {
-      return false;
-    }
-    throw Exception('Could not parse bool from $this');
-  }
+class BuxEntity extends Equatable {
+  final String apiKey;
+  final String? apiSecret;
+  final bool sandbox;
+  final String clientId;
+
+  const BuxEntity({
+    required this.apiKey,
+    this.apiSecret,
+    required this.sandbox,
+    required this.clientId,
+  });
+
+  @override
+  List<Object?> get props => <Object?>[apiKey, apiSecret, sandbox, clientId];
 }
 
-class Bux {
+class Bux extends BuxEntity {
   static const String production_base_url = 'https://api.bux.ph/v1/api';
   static const String sandbox_url = 'https://api.bux.ph/v1/api/sandbox';
 
+  /// Bux Credentials can be passed in via the constructor
+  ///
+  /// or via the environment variables
+  ///
+  /// as **BUX_API_KEY, BUX_API_SECRET, BUX_SANDBOX, BUX_CLIENT_ID**
+  ///
+  /// to be Used with dart package [envify](https://pub.dev/packages/envify) for secure environment variables
+  const Bux({
+    required String apiKey,
+    String? apiSecret,
+    required bool sandbox,
+    required String clientId,
+  }) : super(
+          apiKey: apiKey,
+          apiSecret: apiSecret,
+          sandbox: sandbox,
+          clientId: clientId,
+        );
+
   /// Bux Checkout Api
+  ///
   ///
   /// For more information on the Bux Checkout Api, please visit:
   ///
   /// https://developers.bux.ph/#tag/Generate-Checkout-URL
-  static Future<String?> checkout(CheckoutPayload payload) async {
+  Future<String?> checkout(CheckoutPayload payload) async {
     final BuxClient buxClient = BuxClient();
     const String api_endpoint = '/open/checkout';
 
+    final Map<String, String> headers = _setHeaders();
     return buxClient.post(
       _baseUrl(),
       api_endpoint,
       payload.toJson(),
-      Headers.setDefault(),
+      headers,
     );
   }
 
-  static String _baseUrl() {
-    final String sandbox = env['BUX_SANDBOX']!;
-    final bool isSandBox = sandbox.parseBool();
+  String _baseUrl() {
     late final String base_url;
 
-    if (isSandBox) {
+    if (sandbox == true) {
       base_url = sandbox_url;
     } else {
       base_url = production_base_url;
     }
     return base_url;
+  }
+
+  Map<String, String> _setHeaders({
+    bool acceptJson = true,
+    bool withApiKey = true,
+    Map<String, String>? headers,
+  }) {
+    headers ??= <String, String>{};
+    if (acceptJson == true) {
+      final Map<String, String> accept = <String, String>{
+        'Accept': 'application/json'
+      };
+      headers.addAll(accept);
+    }
+
+    if (withApiKey == true) {
+      final Map<String, String> headerApiKey = <String, String>{
+        'x-api-key': apiKey,
+      };
+      headers.addAll(headerApiKey);
+    }
+    return headers;
   }
 }
