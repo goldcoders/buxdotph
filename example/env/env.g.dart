903ca69d7e21a80fd8e9b24a66a45cfe6d2c2e00
Goldcoders <goldcoders@protonmail.com>
2021-12-02 07:31:16 +0800

    added example for envify

---
+++ example/env/env.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnvifyGenerator
+// **************************************************************************
+
+class _Env {
+  static const bool buxSandbox = true;
+  static const String buxApiKey = 'e0a7f06f2f606a54398316d0b91f1766';
+  static const String buxClientId = '0000018189';
+  static const String buxApiSecret = 'd47f3e582e59';
+}
