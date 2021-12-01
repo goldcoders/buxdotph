3fa8f5a6e03e8c853f83a010a1d9535638f6fc8d
Goldcoders <goldcoders@protonmail.com>
2021-12-02 05:51:17 +0800

    remove not needed export and import

--- lib/buxdotph.dart
+++ lib/buxdotph.dart
@@ -1,10 +1,6 @@
 /// A dart client for Bux API.
 library buxdotph;
 
-export 'package:dotenv/dotenv.dart' show load, env;
-
 export 'src/bux.dart';
-export 'src/constants/env.dart';
-export 'src/headers.dart';
 export 'src/models/channels.dart';
 export 'src/models/checkout_payload.dart';
